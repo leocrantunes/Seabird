34c269325b085249edccc1bb3d244df270c95a49
Xiaolong Li <lixiaolong0328@gmail.com>
2020-02-21 11:44:28 -0500

    Add project files.

---
+++ Seabird/Aircraft.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seabird
+{
+    public sealed class Aircraft : IAircraft
+    {
+        int height;
+        bool airborne;
+        public Aircraft()
+        {
+            height = 0;
+            airborne = false;
+        }
+        public void TakeOff()
+        {
+            Console.WriteLine("Aircraft engine takeoff");
+            airborne = true;
+            height = 200; // Meters
+        }
+        public bool Airborne
+        {
+            get { return airborne; }
+        }
+        public int Height
+        {
+            get { return height; }
+        }
+    }
+}
---
+++ Seabird/IAircraft.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seabird
+{
+    // ITarget interface
+    public interface IAircraft
+    {
+        bool Airborne { get; }
+        void TakeOff();
+        int Height { get; }
+    }
+}
